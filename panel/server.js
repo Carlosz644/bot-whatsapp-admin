69b45616f01e20d98d4b95b8ba34c5ae66c926d5
Carlosz644 <nuevoemail@example.com>
2026-04-02 15:56:22 -0600

    Fix puerto 8080

--- panel/server.js
+++ panel/server.js
@@ -5,7 +5,7 @@ import { crearLicencia, desactivarLicencia, todasLasLicencias, generarCodigo } f
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use('/admin', express.static('./admin'))
