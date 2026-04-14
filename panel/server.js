169ce62a41c4b4cab2f2141d278e97aac2c59853
Carlosz644 <nuevoemail@example.com>
2026-04-14 01:29:58 -0600

    Fix private key array

--- panel/server.js
+++ panel/server.js
@@ -9,47 +9,49 @@ const app = express()
 app.use(express.json())
 app.use(express.static(__dirname))
 
-const privateKey = `-----BEGIN PRIVATE KEY-----
-MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCdp1rGFou6O9Pz
-xwaI6ebk5cGqzSJyFnOZM1NnjTfhMX55Vo/D/WB58LLUDV5BjRUhntwDZLLehJqc
-sm6KZZPTNLQeQiJphIH2MAZxdLabmwgt9UmN10Xjbx++K/LkSBwAC/v33k0wyuZo
-47RAJq97CYkKjzTikRsSmkcNbAlj7wZ9Xttkytj8Q4yxSEc9GM4MvAnkkzH2AkNk
-DWw9zyJ/zpmYSsED/N10/7M0zBucpyIhn62taCPeKvL8aA6+byTOazxvI/E9M6C6
-AX5Dt4KK/7G9b3uIxT8u+KmUe4WX1RlZsX/CTGMvlXkyWE2VI446kozkRT4TUS/Y
-sZVDK+r9AgMBAAECggEACKgMSW0qfe7UDAIlHzZsqavd7vF1jzshXMufLEPqShCs
-J6Bzo2jRt9BaGV+kicfj3aU54mvQkHxKewknHa1HKSUvMKwj9JREiI6JhAhLEI86
-Kz/ZKpMj8MGExi3ml9dHxk7iJnGL3n59mUvLdWxKXWCgKDbFyhz6lhJnjVqdx2Fd
-8ytJXcsbaNRQCPHh/HazVpJ0wPXyDj0MskLQu7LhnK7DPl4nvACXCM5OQpiXBX1M
-CXGJLMVcSMSSGeRDK941IfhMJ7VSlJnjQWVN5Rrobuf9QnjYF7iehUlWEtwjPzzK
-Ra4A1a0fuBQZDjbFmqk9oUVcKQs7A9DSe8I7mBK9wKBgQDLFZQ/KE4AB+DZRe7Dg
-J72DVkTK5BAow5V4Hjykyv5uEI7PF0YtaKyuGmegsVVND3Ug5G26ZB4bxzoHEY8f
-FjStl55I/xKQ28V3ShWAWGsvFy3HtC2qHzpCO14Xha2udrh9d5ciIGK7LIt7NFa2
-GFR2h9Rg3l2DI9o7LMizjYl1wKBgQDGu2aMXb901Bt/pWgIPy8Np2ZtLY+6o6eQ
-FYY/XZ6McXD8EaEIFfxUZaDIZ2kVTBw4Znz+d4Ntz2n/Y1ONTgmMv9IFPbUoVYaE
-SZPQSDHSqoOy+CBSiNT+HT/FOZxF9jyCTGR4jhQhrDms6OJgqn6gCIRO/zfgDiR1
-xZcpC8MzSwKBgQC3xp1S0fxT1s1IkRpR3JD9BHM4/9EYTPXqDKomibvfzThSNnvg
-om3K4Uri4GAGjLHvH+i6532PHq2/9eYxUi1m8RVo9oGWCpP56xpXSgTDtekI5V/m
-C26Ny0BqDmrrjCBhHofMoNLpjuxm1slVNj5LPeHdd+ZwR0l3n8szkfw6HQKBgD+V
-JbIzLFZMpp+oZCh/TqansHWt5hZo1eubd2A+q8NzaBq96S+VGS5HdbGopE5UE5NX
-9xXTVxGDEv7K4KiNFzEZDjDvFU7aTjd08v3om0gzlf9ks7K0ZLEI2qZXUFBx/9oQ
-ZvTLQFWlrK5NtGAJLIo3L122+kGNRM0JctuHD+URAoGABqUEMnBcMEELT6iTbfej
-cRsoJg0zxJLKex6KcGj70VBCUBjhmVF4fwdE6cQGmySO4UtqiVVrZHjeyyp6T2bq
-6XqGnsugi+LsWd0x3I2AZq0VlMp7YSra3QhI1kBqNbsHCp8+zRyWOPpx/jvIBCIn
-E9Kewg+Ku+VR0M8gJ8lhh+g=
------END PRIVATE KEY-----`
+const PRIVATE_KEY = [
+    '-----BEGIN PRIVATE KEY-----',
+    'MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCgyb/crFvC7OG7',
+    'gX3siCYISTxiD3oKzCDHk9Go+71QVKn25jOyum0OGeR+GIMYFZ75LO7izPfJ1qxB',
+    'dBMGKQDssIYKKnb32ijxlj3WaHZTWh19OPVcqazpXcjnW+CVLHLIQLSOq6ynmUDU',
+    'fYkNLW6kmy/R850LynkOWZ6PxJuB2EXcpQAvhbMdVMdOyovUob+2Pvg+nz/QhsJi',
+    'fhNvQQCLIZUufGczt+97NPNDb0nykD2IF1RsY/2R57YWsHrH+oP+Je5Xth4rkT+H',
+    '3RngTh07CTBhNk5c58U9pjImy1jWZ00IA/ABTi/khH3uUMmBaCTX2SpNTK7sqxvT',
+    'Bme8gsWlAgMBAAECggEAM8nn33cwsy9DYfbKjYYo4eBc76TWhWXMZRwqmshC6P3s',
+    'JNdu7XF4sOMMIsVYbEiChPXBGBI2+a8Imqh9fC1228Xb1qho21pMd8wRrN7GxgAv',
+    'oWbYOPl59Uy86jBAvSus6O1WeuG24eDN7eNlYnYwvhoyI+jYu7SCm2moVdxYzlBN',
+    'Lu02Tu3YXFik7zg2WVvn2UJMOQykGD4ALEabgNFodeWCneAutYmTZH6AnBBq3AAt',
+    'h9xsapuJHVHvir+SpEG1b2vNJQxYJigytHgtNbTWjSAQe9ZgMbb8IvLdC5+KblDp',
+    'IRrhqFHh17n8mbSs2BSyksmDXU260P3SP1aZDM/cfQKBgQDPFROuxl4bflScniC5',
+    '+HEupY1vhaCIobYoJUh+qCKe0AOjGuGQnGkoFHN5w/RQlFjx2x5UGOlOwZG1XBjI',
+    'n+XKM8hieLB6gGPh6Xz/7YZVdVMnuVhhbJEDDPiIzN5sjpCmAYyA8rdXGh/q74QK',
+    'Al4G2Cu3Y1I/yJSUJ9OsRs0vawKBgQDGxRzzlyolzUqXCgFNBTbMNYwaSP+4a9n/',
+    'DUiyY1i5Uwn0BkE+vuN2mEYTY7IkpWd1r1mngPInjqcTuXOAraY5M2MOuqIt1/ja',
+    '7J+nS87Y+1HEpU9H+qUnF4zVFuCc5UMExVdxvBdcO8ZzlrfPKwBVnzCbNC96fudB',
+    'AwcRhh5zLwKBgQCDQqPgQ8LBAJmXULAD0VPEspFtIDF6ia5R0hgamG6hZc74YwCZ',
+    'TltlWVk81JWXQgEDBNY7jjLMhevbHk5jVN8uBRa+PCv46krllr9x5dghcSXNZzF7',
+    'R3q/iUg8RkeyXceYQiESV0cZQej7XOjAA3IgOoOzUZR/858iLgCBEbvbkwKBgC1a',
+    'yNJkSb+6aiywob9bUzeheEh/I6qHIYTrGQ1rUdFnuzutObIBiJGFCWhfNUTbYRIf',
+    'YsuvxpF0IXeEt3BDL0yaBx5TQ7JGN4hsublIuPgV0ICYX9DCxnB3FTGCONpUDcjG',
+    '5r8S9hmbohq551MkMr1Wx5aCf9rpngbdk/1Rpx0NAoGAGnza+4FRvKCUhoTuhBZ0',
+    '0iNdhj4kAYMiloImPpfL1Xl41Rkp+R4cN9yl+Ajmanhp61xj515iJ/uYvAiWh8gB',
+    'hGURF/k0vC9KpSdxcjiPm6CZrb5Tmp6uMnEQdLPlbjGrrbyIlvPjHdHf+wCEN2+h',
+    'TR3E741zrO17VTFHg0T/Wr0=',
+    '-----END PRIVATE KEY-----'
+].join('\n')
 
 const firebaseKey = {
-    type: "service_account",
-    project_id: "bot-whatsapp-licencias",
-    private_key_id: "48aa931b0871fbf2b7ed623a3e9a340f90339f1c",
-    private_key: privateKey,
-    client_email: "firebase-adminsdk-fbsvc@bot-whatsapp-licencias.iam.gserviceaccount.com",
-    client_id: "106337392086475541472",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40bot-whatsapp-licencias.iam.gserviceaccount.com",
-    universe_domain: "googleapis.com"
+    type: 'service_account',
+    project_id: 'bot-whatsapp-licencias-5dcbf',
+    private_key_id: '783aacf30b64e23de99a65be0e52e95b0e395138',
+    private_key: PRIVATE_KEY,
+    client_email: 'firebase-adminsdk-fbsvc@bot-whatsapp-licencias-5dcbf.iam.gserviceaccount.com',
+    client_id: '106900626375135790130',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40bot-whatsapp-licencias-5dcbf.iam.gserviceaccount.com',
+    universe_domain: 'googleapis.com'
 }
 
 let db
